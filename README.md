faa50c7ea1f945ce4a8701f97ea0a73f90cde833
DESKTOP-65SAUST\Alumno <ricardogracia2002@gmail.com>
2024-11-21 15:48:39 +0100

    Commit Clase 21-11 autoslide

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-Cambia lo de la propiedad block a clases
+NUNCA DEJARLE EL DEBUGGER PUESTO EN EL CÓDIGO, ESTO QUITA PUNTOS
+
+debugger;
+
+Si antes de una función con un parámetro establecido pero sin valores usamos un /** automaticamente hace referencia a este parámetro para que podamos decirle qué tipo de valores tendrá y demás info
+
+La función setInterval nos permite automatizar un proceso en un determinado tiempo seleccionado en ms.
+
+setInterval(proceso, tiempoenmilisegundos) https://www.w3schools.com/jsref/met_win_setinterval.asp 
+
+El setInterval, al pasarle un proceso (función a realizar), si esa función tiene unos parámetros ya establecidos hay que declarar una función anónima y dentro de esa llamar a la función con el parámetro establecido. *MIRAR LÍNEA 94 slideShow.js*
+En caso de que la función no tenga un parámetro establecido basta con llamarla sin los paréntesís. *Mirar línea 98 slideShow.js*
+
+Tenemos que definir los eventos de click sobre los puntos (dot) dentro del archivo slideShow.js
+
+1) Eliminamos los eventos del DOM (HTML) de onclick en los puntos.
+2) Sacamos la variable arrDots fuera de la función showSlide(), la podemos poner junto el slideIndex y autoSlide.
+3) Recorremos el array arrDots con un bucle for (o con forEach) y a cada dot le asociamos un evento click (con addEventListener).
+4) Dentro del evento tendremos que llamar a las funciones en orden:
+    - stopAutoSlide()
+    - currentDotSlide()
+    - startAutoSlide()
+
+
+EXTRA: 
+Añadimos más slides y más puntos.
--- js/slideShow.js
+++ js/slideShow.js
@@ -7,12 +7,18 @@
  * 
  * También será automático y podremos ir viendo cada slide pasado un cierto tiempo
 */
- 
+
 //Inicializamos el contador
 
 let slideIndex = 1;
+let autoSlide;
+let arrDots = document.querySelectorAll(".dot");
 
 showSlide(slideIndex);
+
+//Iniciamos la función del autoSlide al inicializar la web
+
+startAutoSlide();
 
 // Seleccionamos las flechas de next y prev slide
 
@@ -20,11 +26,18 @@ let nextSlideArrow = document.querySelector(".next");
 let prevSlideArrow = document.querySelector(".prev");
 
 nextSlideArrow.addEventListener("click", function(){
+    stopAutoSlide();
     nextPrevSlide(1);
+    startAutoSlide();
 })
 prevSlideArrow.addEventListener("click", function(){
+    stopAutoSlide();
     nextPrevSlide(-1);
+    startAutoSlide();
 })
+
+//Eventos click en puntos
+
 
 /**
  * ¡¡Crear un apartado de documentación como este con el "/**" automaticamente hace referencia a los parámetros de la función creada debajo!!
@@ -41,6 +54,17 @@ function nextPrevSlide(index) {
 
 /**
  * 
+ * Esta función actualiza la variable slideIndex igualando su valor al índice del punto (dot) que le llegará como parámetro.
+ * 
+ * @param {number} dotIndex 
+ */
+function currentDotSlide(dotIndex) {
+    slideIndex = dotIndex + 1;
+    showSlide(slideIndex);
+}
+
+/**
+ * 
  * @param {number} slideNumber 
  * 
  * Esta función mostrará el slide correspondiente a la posición que le llegue
@@ -49,7 +73,6 @@ function nextPrevSlide(index) {
  */
 function showSlide(slideNumber) {
     let arrSlides = document.querySelectorAll(".mySlides");
-    let arrDots = document.querySelectorAll(".dot");
 
     //Si nos pasamos de ir para adelante o detrás, hacemos unas condiciones que nos aseguren mantenerse siempre en los valores asociados al array.
     if (slideNumber > arrSlides.length) {
@@ -69,3 +92,21 @@ function showSlide(slideNumber) {
     arrSlides[slideIndex - 1].style.display = "block";
     arrDots[slideIndex - 1].className += " active";
 }
+
+// Automatizamos la llamada a la función nextPrevSlide() usando la función built-in setInterval()
+// Le pasamos el parámetro con el valor 1 para que vaya añadiendolé uno a la función nextPrevSlide y por ende las slides vayan avanzando
+
+function startAutoSlide() {
+    autoSlide = setInterval(function(){nextPrevSlide(1); }, 2000);
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlide);
+}
+
+arrDots.forEach(addEventListener("click", function(){
+    stopAutoSlide();
+    currentDotSlide();
+    startAutoSlide();
+}));
+
